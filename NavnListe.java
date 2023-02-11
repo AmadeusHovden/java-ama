b56332875960f7303358f8ca6c425670f70dd719
AmadeusHovden <amadeusmh@uia.no>
2023-02-11 17:39:27 +0100

    sss

--- NavnListe.java
+++ NavnListe.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class NavnListe {
     public static void main(String[] args) {
@@ -10,10 +11,44 @@ public class NavnListe {
         navn.add("Pet");
         navn.add("Sil");
 
-        for (String element : navn) {
-            if (element.contains("a"))
-                System.out.println(element);
+        // while
+        int i = 0;
+        while (i < navn.size()) {
+            System.out.println(navn.get(i));
+            i++;
         }
-    }
 
+        /*
+         * // iterator
+         * Iterator<String> Iterator = navn.iterator();
+         * while (iterator.hasNext()) {
+         * System.out.println(iterator.next());
+         * }
+         * 
+         * // fjerne
+         * Iterator<String> Iterator = navn.iterator();
+         * while (iterator.hasNext()) {
+         * String name = iterator.next();
+         * if (navn.equals("Sil")) {
+         * iterator.remove();
+         * }
+         * }
+         */
+
+        // legge til
+        Iterator<String> iterator = navn.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            if (name.equals("Sil")) {
+                navn.add("Bjørnar");
+                break;
+            }
+        }
+
+    }
 }
+
+/*
+ * navn.set(2, "Lars");
+ * navn.remove(0);
+ */
