256eab3ab74930596f5d7fcb76288531a4731110
AmadeusHovden <amadeusmh@uia.no>
2023-02-11 11:30:42 +0100

    sss

---
+++ NavnListe.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class NavnListe {
+    public static void main(String[] args) {
+        ArrayList<Integer> navn = new ArrayList<>();
+
+        navn.add(2);
+        navn.add(2);
+
+        System.out.println(navn);
+
+    }
+}
