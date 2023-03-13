15faf84a44321f0521a6cebe9542a8460059d9a4
AmadeusHovden <amadeusmh@uia.no>
2023-03-13 20:28:55 +0100

    sss

---
+++ encaps.java
@@ -0,0 +1,21 @@
+public class encaps {
+
+    private String navn;
+    private int alder;
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+}
